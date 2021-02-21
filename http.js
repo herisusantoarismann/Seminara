7953b240e590f355fa3c880d1c8ab6076ce2470b
Heri Susanto Arisman <69309194+herisusantoarismann@users.noreply.github.com>
2021-02-21 13:07:17 +0700

    Add files via upload

---
+++ http.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const api = require("../server");
+const config = require("../config");
+
+const cfgenv = config.env;
+
+process.env.NODE_PATH = __dirname;
+
+async function startServer() {
+  console.log(`starting up ${cfgenv.appname}...`);
+  console.log(`using config: ${JSON.stringify(cfgenv)}`);
+
+  // initialize dependencies
+  await api.deps.prepare();
+  console.log("dependency initialized");
+
+  // run server
+  const server = api.server();
+  const port = process.env.PORT || 5000;
+  server.listen(port, () =>
+    console.log(`server started on port ${cfgenv.server.port} \n!-`)
+  );
+}
+
+startServer();
